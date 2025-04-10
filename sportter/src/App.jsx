96e447577856e2c4500c3034fab3b9831b6a4d6c
DanielEstebanM <131672025+DanielEstebanM@users.noreply.github.com>
2025-04-10 20:01:50 +0200

    Reseteo reseteo

---
+++ sportter/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Inicio from './components/PantallaInicio'
+
+function App() {
+
+  return (
+    <>
+    <h2>Hola</h2>
+    <PantallaInicio></PantallaInicio>
+    </>
+  )
+}
+
+export default App
---
+++ sportter/src/components/PantallaInicio.jsx
@@ -0,0 +1,9 @@
+function PantallaInicio() {
+  return (
+    <div>
+      <h2>Hola</h2>
+    </div>
+  )
+}
+
+export default PantallaInicio
