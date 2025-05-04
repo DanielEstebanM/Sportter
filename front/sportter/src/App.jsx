1b58bbfef163b08b7b85559a0660618f8bd899c1
DanielEstebanM <131672025+DanielEstebanM@users.noreply.github.com>
2025-05-04 19:19:31 +0200

    PantallaInicio

--- front/sportter/src/App.jsx
+++ front/sportter/src/App.jsx
@@ -1,9 +1,17 @@
-
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PantallaInicio from "./components/PantallaInicio";
+import PantallaPrincipal from "./components/PantallaPrincipal";
 
 function App() {
-  return <PantallaInicio />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<PantallaInicio />} />
+        <Route path="/principal" element={<PantallaPrincipal />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
