ce3888671a5350e80fd2dfffea7a4cc781cc4419
Sarita <126904928+Savxrz@users.noreply.github.com>
2025-05-14 19:04:22 +0200

    hhh
    bjb

--- front/sportter/src/services/api.js
+++ front/sportter/src/services/api.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 export const loginUser = async (credentials) => {
     try {
       console.log("Enviando credenciales:", credentials);
@@ -19,19 +18,6 @@ export const loginUser = async (credentials) => {
       throw new Error(error.response?.data?.message || "Credenciales incorrectas");
     }
   };
-=======
 
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api';
-
-export const loginUser = async (email, password) => {
-  try {
-    const response = await axios.post(`${BASE_URL}/login`, { email, password });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
->>>>>>> Stashed changes
